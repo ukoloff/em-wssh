85844756bcce73e7f7a3a1dd5322a3633493238f
Stas Ukolov <ukoloff@gmail.com>
2015-02-13 15:35:35 +0500

    Parse command line

--- server.rb
+++ server.rb
@@ -1,3 +1,39 @@
 require 'yaml'
 require 'getoptlong'
 require 'em-websocket'
+
+opts = GetoptLong.new(
+  ['-l', '--listen', GetoptLong::REQUIRED_ARGUMENT],
+  ['-d', '--daemon', GetoptLong::NO_ARGUMENT],
+)
+
+def help
+  puts <<-EOF
+wssh - proxy ssh thru websocket
+
+Usage: #{File.basename __FILE__} [options...]
+
+  -l --listen=port Listen to port
+  -d --daemon      Run daemonized
+  -h --help        Show this help
+EOF
+  exit 1
+end
+
+port = 4567
+daemon = false
+
+begin
+  opts.each do |opt, arg|
+    case opt
+    when '-d'
+      daemon=true
+    when '-l'
+      port=arg
+    end
+  end
+rescue
+  help
+end
+
+puts "Running d=#{daemon} on port #{port}"
