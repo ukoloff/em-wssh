2d95d74c52085ea740ccc9d983a6dadde7a387ea
Stas Ukolov <ukoloff@gmail.com>
2015-02-20 15:31:07 +0500

    Working HTTP CONNECT proxy

--- connect.rb
+++ connect.rb
@@ -73,3 +73,77 @@ def pid
 end
 
 pid
+
+module HttpConn
+  def post_init
+    port, ip = Socket.unpack_sockaddr_in(get_peername)
+    log "Client connected from #{ip}:#{port}"
+  end
+
+  def wssh data
+    @ws.send data.unpack 'C*'
+  end
+
+  def receive_data data
+    return wssh data if @pass
+
+    if @buf
+      @buf<<data
+    else
+      @buf=data
+    end
+    while m=/\r?\n/.match(@buf)
+      @buf=m.post_match
+      receive_line m.pre_match
+    end
+  end
+
+  def receive_line line
+    @n||=0
+    @n+=1
+    if @n>1
+      connect_wssh if 0==line.length
+    else
+      parse line
+    end
+  end
+
+  def connect_wssh
+    @pass = true
+    send_data "HTTP/1.0 200 Ok\r\n\r\n"
+
+    log "Redirect to", uri="#{ARGV[0]}/#{@wssh}"
+    @ws = Faye::WebSocket::Client.new uri
+
+    @ws.on :open do |event|
+      log "Connected to WSSHD"
+      wssh @buf
+      @buf=nil
+    end
+
+    @ws.on :message do |event|
+      send_data event.data.pack 'C*'
+    end
+
+    @ws.on :close do
+      log "Websocket closed"
+      close_connection
+    end
+  end
+
+  def parse line
+    verb, dst = line.split
+    return @wssh = dst[1] if 'connect'==verb.downcase and dst=/^([-.\w]+):22$/.match(dst)
+    send_data "HTTP/1.0 500 Bad request\r\n\r\n"
+    close_connection
+  end
+
+  def unbind
+    log "Client disconnected"
+    @ws.close if @ws
+  end
+end
+
+EM.run do
+  EM.start_server "127.0.0.1", port, HttpConn
+end
