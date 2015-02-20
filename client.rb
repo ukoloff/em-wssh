de7e7da7dd772a97e0f2c626e9d327e06127a580
Stas Ukolov <ukoloff@gmail.com>
2015-02-20 14:26:32 +0500

    Working client

--- client.rb
+++ client.rb
@@ -13,3 +13,48 @@ Usage: #{File.basename $0} ws[s]://host[:port]/uri
 EOT
   exit
 end
+
+STDOUT.sync=true
+
+module Stdio
+  attr_accessor :buf
+
+  def initialize(websocket)
+    @ws = websocket
+  end
+
+  def ws_send data
+    @ws.send data.unpack 'C*'
+  end
+
+  def receive_data data
+    if buf
+      buf.push data
+    else
+      ws_send data
+    end
+  end
+
+  def unbind
+    EM.stop_event_loop
+  end
+end
+
+EM.run do
+  ws = Faye::WebSocket::Client.new ARGV[0]
+  stdio = EM.attach $stdin, Stdio, ws
+  stdio.buf = []
+
+  ws.on :open do |event|
+    stdio.buf.each{|data| ws.ws_send data}
+    stdio.buf=nil
+  end
+
+  ws.on :message do |event|
+    print event.data.pack 'C*'
+  end
+
+  ws.on :close do
+    EM.stop_event_loop
+  end
+end
