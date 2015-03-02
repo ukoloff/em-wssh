a73d8c1e4a1686fc7450fab80d3b476508fc89a8
Stas Ukolov <ukoloff@gmail.com>
2015-03-02 22:36:03 +0500

    Fix regexp

--- lib/em/wssh/exe.rb
+++ lib/em/wssh/exe.rb
@@ -3,7 +3,7 @@ require_relative '../wssh'
 module Exe
   def self.do!
     cmd = ARGV.shift
-    throw "Invalid command '#{cmd}'" unless /^\w+$/.match cmd
+    throw "Invalid command '#{cmd}'" unless /\A\w+\Z/.match cmd
     require_relative cmd
   end
 end
