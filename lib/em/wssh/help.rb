1ea9e35c520162d48243532addbb529245faa2e6
Stas Ukolov <ukoloff@gmail.com>
2015-03-04 12:26:55 +0500

    Help for individual commands

--- lib/em/wssh/help.rb
+++ lib/em/wssh/help.rb
@@ -1,4 +1,4 @@
-require_relative 'all'
+require_relative 'exe'
 
 module EventMachine::Wssh
 module Help
@@ -6,6 +6,16 @@ module Help
   Title='Show this help'
 
   def self.go!
+    if mod = Exe.command?(ARGV.shift)
+      command mod
+    else
+      top
+    end
+  end
+
+  def self.top
+    require_relative 'all'
+
     puts <<-EOT
 WSSH suite v#{VERSION}
 
@@ -15,6 +25,14 @@ Available commands:
 
     EOT
     Exe.commands.each{|cmd, mod| puts "  wssh #{cmd}\t#{mod::Title rescue nil}"}
+  end
+
+  def self.command mod
+    if mod.respond_to? :help
+      mod.help
+    else
+      puts mod::Title
+    end
   end
 end
 end
